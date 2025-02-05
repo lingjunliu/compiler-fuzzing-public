cec2defcbb82eaae0fe4657f40995075d61cfbfd
sodiumrim <nanarimlee@gmail.com>
2025-02-05 11:49:32 -0500

    Get the first variable assignment call

--- mutators/109919.sh
+++ mutators/109919.sh
@@ -9,8 +9,8 @@ fi
 file="$1"
 SEED=$2
 
-shortVars=$(grep -noE "short [a-zA-Z_][a-zA-Z0-9_]* = [0-9]+;" "$file" | \
-sed -E 's/short ([a-zA-Z_][a-zA-Z0-9_]*) = [0-9]+;/\1/')
+shortVars=$(grep -noE "short [a-zA-Z_][a-zA-Z0-9_]* = 0;" "$file" | \
+sed -E 's/short ([a-zA-Z_][a-zA-Z0-9_]*) = 0;/\1/')
 
 if [ -z "$shortVars" ]; then
   echo "No matching patterns found."
@@ -23,14 +23,15 @@ shortVar=$(echo "$random_match1" | cut -d: -f2)
 
 sed -i -E "${line1}d" "$file"
 
-calls=$(grep -noE "[^a-zA-Z0-9_]${shortVar}[^a-zA-Z0-9_]" "$file")
+calls=$(grep -noE "[^a-zA-Z0-9_]${shortVar} = " "$file")
 
 if [ -z "$calls" ]; then
   echo "No matching patterns found."
   exit 0
 fi
 
-random_match2=$(echo "$calls" | awk -v seed="$SEED" 'BEGIN {srand(seed); line=""} {if (rand() <= 1/NR) line=$0} END {print line}')
-line2=$(echo "$random_match2" | cut -d: -f1)
+call=$(echo "$calls" | head -n 1)
+
+line2=$(echo "$call" | cut -d: -f1)
 
 sed -i -E "${line2}s/[^a-zA-Z0-9_]${shortVar}[^a-zA-Z0-9_]/ short ${shortVar} /" "$file"
