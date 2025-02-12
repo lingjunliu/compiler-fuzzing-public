8d8be7f6ed64a10b1040fe01e660a73ed28ea5c4
sodiumrim <nanarimlee@gmail.com>
2025-02-12 01:50:28 -0500

    Generalized to all the for loops

--- mutators/108793.sh
+++ mutators/108793.sh
@@ -8,7 +8,7 @@ fi
 
 file="$1"
 
-match=$(grep -noE '(for \([a-zA-Z_][a-zA-Z0-9_]* \*[a-zA-Z_][a-zA-Z0-9_]* = &[a-zA-Z_][a-zA-Z0-9_]*\[[0-9]+\]; [a-zA-Z_][a-zA-Z0-9_]* \!= &[a-zA-Z_][a-zA-Z0-9_]*\[[0-9]+\]; [a-zA-Z_][a-zA-Z0-9_]*\+\+\))' "$file")
+match=$(grep -noE '(for \(.*\))' "$file")
 line=$(echo "$match" | cut -d: -f1)
 
 sed -i -E "${line}s/\!=/>/" "$file"
