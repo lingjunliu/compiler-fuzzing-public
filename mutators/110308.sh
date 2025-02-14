57ab8b5a5f608d2353b0ae3b936bf2147da8b776
sodiumrim <nanarimlee@gmail.com>
2025-02-14 14:54:48 -0500

    Edited based on the suggestion

--- mutators/110308.sh
+++ mutators/110308.sh
@@ -7,7 +7,20 @@ if [ $# -ne 2 ]; then
 fi
 
 file="$1"
+SEED="$2"
 
-#random struct
-#structName s; --> structName();
-sed -i -E 's/(void encodeBlock\(\) \{ ms_adpcm_state) [a-zA-Z_][a-zA-Z0-9_]*(; \})/\1\(\)\2/' "$file"
+structNames=$(grep -oE 'struct [a-zA-Z_][a-zA-Z0-9_]*' "$file" | \
+sed -E 's/struct ([a-zA-Z_][a-zA-Z0-9_]*)/\1/')
+
+# If no matches are found, exit
+if [ -z "$structNames" ]; then
+  echo "No matching patterns found."
+  exit 0
+fi
+
+echo "$structNames"
+
+structName=$(echo "$structNames" | awk -v seed="$SEED" 'BEGIN {srand(seed); line=""} {if (rand() <= 1/NR) line=$0} END {print line}')
+echo "$structName"
+
+sed -i -E "s/${structName} [a-zA-Z_][a-zA-Z0-9_]*;/${structName}\(\);/" "$file"
