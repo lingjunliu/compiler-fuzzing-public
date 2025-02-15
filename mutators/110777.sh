d17d61eb75c224e3bbf861f6cea05144f88c35f8
sodiumrim <nanarimlee@gmail.com>
2025-02-15 08:32:21 -0500

    Modified to use a pattern instead of deleting the whole thing using .*

--- mutators/110777.sh
+++ mutators/110777.sh
@@ -9,7 +9,6 @@ fi
 file="$1"
 SEED=$2
 
-#Use the pattern instead of removing with .*
 sed -i -E 's/(unsigned [a-zA-Z_][a-zA-Z0-9_]*) = 0;/\1;/g' "$file"
 
 
@@ -22,7 +21,7 @@ fi
 
 rand_match1=$(echo "$matches1" | awk -v seed="$SEED" 'BEGIN {srand(seed); line=""} {if (rand() <= 1/NR) line=$0} END {print line}')
 line1=$(echo "$rand_match1" | cut -d: -f1)
-sed -i -E "${line1} s/.*//" "$file"
+sed -i -E "${line1} s/[a-zA-Z_][a-zA-Z0-9_]* = \(int \*\)calloc\([0-9]+, sizeof\(int\)\);//" "$file"
 
 
 matches2=$(grep -noE 'free\([a-zA-Z_][a-zA-Z0-9_]*\);' "$file")
@@ -34,7 +33,7 @@ fi
 
 rand_match2=$(echo "$matches2" | awk -v seed="$SEED" 'BEGIN {srand(seed); line=""} {if (rand() <= 1/NR) line=$0} END {print line}')
 line2=$(echo "$rand_match2" | cut -d: -f1)
-sed -i -E "${line2} s/.*//" "$file"
+sed -i -E "${line2} s/free\([a-zA-Z_][a-zA-Z0-9_]*\);//" "$file"
 
 
 matches3=$(grep -noE 'return 0;' "$file")
@@ -46,4 +45,4 @@ fi
 
 rand_match3=$(echo "$matches3" | awk -v seed="$SEED" 'BEGIN {srand(seed); line=""} {if (rand() <= 1/NR) line=$0} END {print line}')
 line3=$(echo "$rand_match3" | cut -d: -f1)
-sed -i -E "${line3} s/.*//" "$file"
+sed -i -E "${line3} s/return 0;//" "$file"
