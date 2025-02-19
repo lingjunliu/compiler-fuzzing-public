954c1580dc705dc980beb00e8f9ce34fb9e2114b
sodiumrim <nanarimlee@gmail.com>
2025-02-19 04:48:08 -0500

    Case 62703

---
+++ mutators/62703.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if a file argument is provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <file> <seed>"
+    exit 1
+fi
+
+file="$1"
+
+sed -i -E "s/return __builtin_rotateleft([0-9]+)\(([a-zA-Z_][a-zA-Z0-9_]*), ([0-9]+)\) \& (0x[A-F0-9]*L?);/\
+return __builtin_rotateleft\1\(\(\2 \& \4\), \3\);/" "$file"
