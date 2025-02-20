463fb5f4c459a94e703fa485a619bffffe8186a3
Manu Jayachandran <mjayach@ncsu.edu>
2025-02-20 16:09:53 -0500

    case 63746

--- mutators/63746.sh
+++ mutators/63746.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Check if a file argument is provided
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <file> <seed>"
     exit 1
@@ -8,4 +6,4 @@ fi
 
 file="$1"
 
-sed -i -E "s/(int ([a-zA-Z_][a-zA-Z0-9_]*)) = 0 \+ 1;/\1 = \2 + 1;/g" "$file"
+sed -i -E  "s/int ([a-zA-Z_][a-zA-Z0-9_]*) = 0 \+ 1;/int \1 = \1 \+ 1;/g" "$file"
