1c44ddd769c1db38f4a3ff43ad0362c6503439de
trkhokha <trkhokha@ncsu.edu>
2025-02-25 09:47:34 -0500

    Fixed identifier for more functionality

--- mutators/63214.sh
+++ mutators/63214.sh
@@ -12,4 +12,4 @@ file="$1"
 
 
 # Replace "return func2(i);" with "[[clang::musttail]] return func2(i);"
-sed -i -E 's/return ([a-zA-Z0-9]+\([a-z]);/[[clang::musttail]] return \1;/' "$file"
+sed -i -E 's/return ([a-zA-Z_][a-zA-Z0-9_]*\([a-zA-Z_][a-zA-Z0-9_]*\));/[[clang::musttail]] return \1;/' "$file"
