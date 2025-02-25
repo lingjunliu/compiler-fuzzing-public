106d4a8c8aef8277e6a55d738a553e8ba6bd3b26
Manu Jayachandran <mjayach@ncsu.edu>
2025-02-25 16:16:54 -0500

    case 70068

---
+++ mutators/70068.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Check if correct number of arguments is provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <file> <seed>"
+    exit 1
+fi
+
+
+file="$1"
+
+sed -i -E 's/_T/t/g' "$file"
