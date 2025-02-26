be68393a613e843a8062ed42f8c33ba0cbc56835
sodiumrim <nanarimlee@gmail.com>
2025-02-26 11:56:04 -0500

    Changed it to just int

--- mutators/64631.sh
+++ mutators/64631.sh
@@ -8,4 +8,4 @@ fi
 
 file="$1"
 
-sed -i -E "s/static int ([a-zA-Z_][a-zA-Z0-9_]*)\(([a-zA-Z_][a-zA-Z0-9_]*)\)/static int __attribute__\(\(target_clones\(\"default,avx\"\)\)\) \1\(\2\)/" "$file"
+sed -i -E "s/int ([a-zA-Z_][a-zA-Z0-9_]*)\(([a-zA-Z_][a-zA-Z0-9_]*)\)/int __attribute__\(\(target_clones\(\"default,avx\"\)\)\) \1\(\2\)/" "$file"
