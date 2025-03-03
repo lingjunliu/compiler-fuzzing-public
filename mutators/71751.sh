d6b00d98b15b2446ae55a48ff34a187385e92d57
sodiumrim <nanarimlee@gmail.com>
2025-03-03 11:34:18 -0500

    Detect (type) only

--- mutators/71751.sh
+++ mutators/71751.sh
@@ -9,4 +9,4 @@ fi
 file="$1"
 SEED=$2
 
-sed -i -E 's/uint8_t ([a-zA-Z_][a-zA-Z0-9_]*) = \(uint8_t\)vqrshrunh_n_s16(\([a-zA-Z_][a-zA-Z0-9_]*, [0-9]+\));/uint8_t \1 = vqrshrunh_n_s16\2;/' "$file"
+sed -i -E 's/\(uint8_t\)//g' "$file"
