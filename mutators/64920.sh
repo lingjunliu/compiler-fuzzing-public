b46974dc95b0ca78c3c3e73e536fa49261d6cd2a
trkhokha <trkhokha@ncsu.edu>
2025-03-05 19:25:47 -0500

    Update 64920

--- mutators/64920.sh
+++ mutators/64920.sh
@@ -12,4 +12,4 @@ file="$1"
 
 
 # Replace "void symbolic_longlong_and_int0(size_t len) {" with "void symbolic_longlong_and_int0(long long len) {"
-sed -i -E 's/(^|[^a-zA-Z0-9_])size_t([^a-zA-Z0-9_]|$)/\1long long\2/g' "$file"
+sed -i -E 's/([^a-zA-Z0-9_])size_t([^a-zA-Z0-9_])/\1long long\2/g' "$file"
