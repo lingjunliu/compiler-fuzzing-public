39efab8d48258b2e73793cf2baf371487e53aada
trkhokha <trkhokha@ncsu.edu>
2025-03-05 19:15:26 -0500

    Update Case 63550

--- mutators/63550.sh
+++ mutators/63550.sh
@@ -11,5 +11,5 @@ fi
 file="$1"
 
 
-# Replace "_Atomic(int *) b = (int *)0;" with "_Atomic(int *) b = 0;"
-sed -i -E 's/_Atomic\(int \*\) ([a-zA-Z_][a-zA-Z0-9_]*) = \(int \*\)0;/_Atomic(int *) \1 = 0;/' "$file"
+# Replace "= (int *);" with """";"
+sed -i -E 's/= \(int \*\)([0-9]+);/=\1;/' "$file"
