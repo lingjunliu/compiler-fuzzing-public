74f2d52d738af05114ce473a8a746049d485eea1
sodiumrim <nanarimlee@gmail.com>
2025-03-20 13:17:14 -0400

    dont't detect the type

--- mutators/81472.sh
+++ mutators/81472.sh
@@ -9,4 +9,4 @@ fi
 file="$1"
 SEED=$2
 
-sed -i -E 's/([a-zA-Z_][a-zA-Z0-9_]*) ([a-zA-Z_][a-zA-Z0-9_]*) alignas\(([0-9]+)\);/\1 alignas\(\3\) \2;/' "$file"
+sed -i -E 's/([a-zA-Z_][a-zA-Z0-9_]*) alignas\(([0-9]+)\);/alignas\(\2\) \1;/' "$file"
