4fff2fc94266c8d97f2e8c718b84118b82b52dce
trkhokha <trkhokha@ncsu.edu>
2025-03-25 17:30:52 -0400

    Update Case 63261

--- mutators/63261.sh
+++ mutators/63261.sh
@@ -13,7 +13,4 @@ file="$1"
 
 # Update mutex acquire and acquired callback function conditions (handling multi-line patterns)
 # :a;N;$!ba: reads the entire input into memory by concatenating all lines into the pattern space.
-sed -i -E ':a;N;$!ba;s/if \(kind == ompt_mutex_test_lock \|\| kind == ompt_mutex_lock \|\|\n *kind == ompt_mutex_test_nest_lock \|\| kind == ompt_mutex_nest_lock\)/if (kind == ompt_mutex_test_lock || kind == ompt_mutex_test_nest_lock)/g' "$file"
-
-# Add a comment to the return statement 
-sed -i -E 's/return 1;/return 1; \/\* non-zero indicates success *\//g' "$file"
+sed -i -E 's/\|\| [a-zA-Z_][a-zA-Z0-9_]* == [a-zA-Z_][a-zA-Z0-9_]*//' "$file"
