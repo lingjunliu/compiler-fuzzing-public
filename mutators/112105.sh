c5ca2530eea16181271df571980bed9a979b5843
sodiumrim <nanarimlee@gmail.com>
2025-03-27 14:09:07 -0400

    Changed to use .*

--- mutators/112105.sh
+++ mutators/112105.sh
@@ -10,8 +10,8 @@ file="$1"
 SEED=$2
 
 # Save the name of the last variable used inside of a vmul_n_f32
-matches=$(grep -oE 'vmul_n_f32\(.*\..*, [a-zA-Z_][a-zA-Z0-9_]*\)' "$file" | \
-sed -E "s/vmul_n_f32\(.*\..*, ([a-zA-Z_][a-zA-Z0-9_]*)\)/\1/")
+matches=$(grep -oE 'vmul_n_f32\(.*, [a-zA-Z_][a-zA-Z0-9_]*\)' "$file" | \
+sed -E "s/vmul_n_f32\(.*, ([a-zA-Z_][a-zA-Z0-9_]*)\)/\1/")
 
 if [ -z "$matches" ]; then
   echo "No matching patterns found."
@@ -37,4 +37,4 @@ value=$(echo "$declaration" | cut -d: -f2)
 
 # Delete the declaration and replace the variable name with the content
 sed -i -E "$line d" "$file"
-sed -i -E "s/vmul_n_f32\((.*\..*,) $scalar\)/vmul_lane_f32\(\1 $value\)/" "$file"
+sed -i -E "s/vmul_n_f32\((.*,) $scalar\)/vmul_lane_f32\(\1 $value\)/" "$file"
