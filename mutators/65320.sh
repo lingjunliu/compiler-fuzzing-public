7dc5d9a5930b416530c26042c47a95a870644d76
trkhokha <trkhokha@ncsu.edu>
2025-04-01 07:02:37 -0400

    Update Case 65320

--- mutators/65320.sh
+++ mutators/65320.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Check if a file argument is provided
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <file> <seed>"
     exit 1
@@ -5,4 +8,4 @@ fi
 
 file="$1"
 
-sed -i -E 's/void verifyfeaturestrings\(void\) \{ \(void\)__builtin_cpu_supports\("f16c"\); \}/void verifyfeaturestrings(void) {\n  (void)__builtin_cpu_supports("f16c");\n  (void)__builtin_cpu_supports("avx512fp16");\n}/g' "$file"
+sed -i 's/(\(void\)__builtin_cpu_supports("f16c"));/\1;  (void)__builtin_cpu_supports("avx512fp16");/' "$file"
