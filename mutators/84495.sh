fb68246bf26131b2a16bbcfccb188f3769f64e97
sodiumrim <nanarimlee@gmail.com>
2025-04-09 11:22:27 -0400

    Made it specific to va_start and deleted any other lines the parameter is used

--- mutators/84495.sh
+++ mutators/84495.sh
@@ -10,8 +10,8 @@ file="$1"
 SEED=$2
 
 # Name of the first parameter
-matches=$(grep -noE '[a-zA-Z_][a-zA-Z0-9_]* [a-zA-Z_][a-zA-Z0-9_]*\([a-zA-Z_][a-zA-Z0-9_]* \*([a-zA-Z_][a-zA-Z0-9_]*)(, .*)*\) \{' "$file" | \
-sed -E 's/[a-zA-Z_][a-zA-Z0-9_]* [a-zA-Z_][a-zA-Z0-9_]*\([a-zA-Z_][a-zA-Z0-9_]* \*([a-zA-Z_][a-zA-Z0-9_]*)(, .*)*\) \{/\1/')
+matches=$(grep -noE '[a-zA-Z_][a-zA-Z0-9_]* [a-zA-Z_][a-zA-Z0-9_]*\(.*[a-zA-Z_][a-zA-Z0-9_]* (\*)?([a-zA-Z_][a-zA-Z0-9_]*), \.\.\.\) \{' "$file" | \
+sed -E 's/[a-zA-Z_][a-zA-Z0-9_]* [a-zA-Z_][a-zA-Z0-9_]*\(.*[a-zA-Z_][a-zA-Z0-9_]* (\*)?([a-zA-Z_][a-zA-Z0-9_]*), \.\.\.\) \{/\2/')
 
 if [ -z "$matches" ]; then
   echo "No matching patterns found."
@@ -26,4 +26,6 @@ paramName=$(echo "$rand_match" | cut -d: -f2)
 sed -i -E "${line} s/([a-zA-Z_][a-zA-Z0-9_]* [a-zA-Z_][a-zA-Z0-9_]*\()[a-zA-Z_][a-zA-Z0-9_]* \*[a-zA-Z_][a-zA-Z0-9_]*(,)? /\1/" "$file"
 
 # Remove parameter reference, keep the last comma if there is one
-sed -i -E "s/(,)?\s*$paramName(,)?/\2/g" "$file"
+#sed -i -E "s/(,)?\s*$paramName(,)?/\2/g" "$file"
+sed -i -E "s/(va_start\(.*), $paramName\)/\1\)/" "$file"
+sed -i -E "/[^a-zA-Z_0-9]$paramName[^a-zA-Z_0-9]/d" "$file"
