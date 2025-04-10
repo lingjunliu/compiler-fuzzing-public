c00980087fe4da362e027d1427704308f533ce14
Sam Fishburn <samfishburn@icloud.com>
2025-04-10 15:31:31 -0400

    Final fix for 85387.sh, more generalized pattern now works.

--- mutators/85387.sh
+++ mutators/85387.sh
@@ -2,5 +2,5 @@
 
 input_file="$1"
 output_file="$2"
-
-sed -E 's/typedef ([a-zA-Z_][ a-zA-Z0-9_]*) ([a-zA-Z_][a-zA-Z0-9_]*);/typedef \1 \2 __attribute__((aligned(4)));/' "$input_file" > "$output_file"
+ 
+sed -E 's/typedef ([a-zA-Z_][a-zA-Z0-9_]*)(( [a-zA-Z_][a-zA-Z0-9_]*)*);/typedef \1\2 __attribute__((aligned(4)));/' "$input_file" > "$output_file"
