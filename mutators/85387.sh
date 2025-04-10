ff4db2b0773a48f828004eeacd7e63cbb2d0bed9
Sam Fishburn <samfishburn@icloud.com>
2025-04-10 14:37:05 -0400

    Fixed regex for 85387.sh

--- mutators/85387.sh
+++ mutators/85387.sh
@@ -3,4 +3,4 @@
 input_file="$1"
 output_file="$2"
 
-sed -E 's/typedef long long Tal4llong;/typedef long long Tal4llong __attribute__((aligned(4)));/' "$input_file" > "$output_file"
+sed -E 's/typedef ([a-zA-Z_][ a-zA-Z0-9_]*) ([a-zA-Z_][a-zA-Z0-9_]*);/typedef \1 \2 __attribute__((aligned(4)));/' "$input_file" > "$output_file"
